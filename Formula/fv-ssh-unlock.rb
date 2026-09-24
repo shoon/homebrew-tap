75a980762110667a81a98297113559797b9d3829
Shaun Murphy <github-relay@thefeeding.com>
2026-09-24 12:37:54 -0400

    fv-ssh-unlock 0.2.0-rc.4

--- Formula/fv-ssh-unlock.rb
+++ Formula/fv-ssh-unlock.rb
@@ -1,16 +1,10 @@
 class FvSshUnlock < Formula
   desc "Monitor and unlock FileVault-protected Macs over SSH"
   homepage "https://github.com/shoon/fv-ssh-unlock"
-  url "https://github.com/shoon/fv-ssh-unlock/archive/refs/tags/v0.2.0-rc.3.tar.gz"
-  sha256 "a2bf9f8b6e76e89f4f027528d76b9cd55f915b8096c4657f1754722bb47b876c"
+  url "https://github.com/shoon/fv-ssh-unlock/archive/refs/tags/v0.2.0-rc.4.tar.gz"
+  sha256 "b29c219dd51f60c8a6de1307f0769635166a52c00760dec101892ad9903a4095"
   license "Apache-2.0"
   head "https://github.com/shoon/fv-ssh-unlock.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/shoon/homebrew-tap/releases/download/fv-ssh-unlock-0.2.0-rc.3"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "9d297af4403e11dbdec3cae3ab5df42cfe289e740fd6a90c5cf50788d5fce955"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1a6f2a9639fff7c49c2cb3672232320ca45e595ab9dcf24f6aba695e4d7ab084"
-  end
 
   depends_on "go" => :build
 
